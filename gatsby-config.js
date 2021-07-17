94ddbff5e10d99564d7d72ccdbd66960ea2dab1a
Lucas S. Silva <luc.silva@g.globo>
2021-07-17 12:43:37 -0300

    Acerta favico e corrige manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,13 +60,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Lucas S. Silva Blog`,
+        short_name: `Lucas`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/ls-icon.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
